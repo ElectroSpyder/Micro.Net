19a0ac7084b7aee6f2f92968b867c6de805ff4b7
Pedro Gonzalez <prgazure@gmail.com>
2022-08-24 14:19:33 -0300

    Creando las primeras clases en proyecto Domain

---
+++ Micro.Net/Micro.Net.Domain/Entities/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Micro.Net.Domain.Entities
+{
+    using Micro.Net.Domain.Common;
+    public class UserEntity : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string? Password { get; set; }
+        public string Email { get; set; }
+    }
+}
